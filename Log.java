4ffbb8f87417a9a913a2fe7f55ef73ee7c77d944
tomaszpik <45516292+tomaszpik@users.noreply.github.com>
2018-12-01 16:40:04 +0100

    Add files via upload

---
+++ Log.java
@@ -0,0 +1,23 @@
+public class Log {
+    public static void main(String[] args) {
+
+
+        //negacje!
+        // koniukcja && (AND)
+        // alternatywa || (OR)
+
+
+        int x=0;
+        int y=5;
+
+        boolean test =(x>0) || (y++ <3);
+
+        System.out.println(test);
+        System.out.println(y);
+
+        int w= 6+(4 /2);
+
+        System.out.println(1+2+"z");  //(1+2) + "z'
+        System.out.println("z"+1+2);
+    }
+}
